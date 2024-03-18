34756bc032cc9dda8f419b9eb190407695624d8a
Muromets812 <63260729+Muromets812@users.noreply.github.com>
2024-03-18 10:42:33 +0300

    Initial commit

---
+++ api_with_restrictions/advertisements/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class AdvertisementStatusChoices(models.TextChoices):
+    """Статусы объявления."""
+
+    OPEN = 'OPEN', 'Открыто'
+    CLOSED = 'CLOSED', 'Закрыто'
+    DRAFT = 'DRAFT', 'Черновик'
+
+
+class Advertisement(models.Model):
+    """Объявление."""
+
+    title = models.TextField()
+    description = models.TextField(default='')
+    status = models.TextField(
+        choices=AdvertisementStatusChoices.choices,
+        default=AdvertisementStatusChoices.OPEN
+    )
+    creator = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,55 @@
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement, AdvertisementStatusChoices, Favorite
+from advertisements.permissions import IsOwner
+from advertisements.serializers import AdvertisementSerializer, FavoriteSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filterset_class = AdvertisementFilter
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
+
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwner()]
+        elif self.action in ["add_favorite", "favorites"]:
+            return [IsAuthenticated()]
+        return []
+
+
+    def get_queryset(self):
+        queryset = Advertisement.objects.all()
+        if not self.request.user.is_authenticated:
+            queryset = queryset.exclude(status=AdvertisementStatusChoices.DRAFT)
+        else:
+            queryset = queryset.filter(creator=self.request.user) | queryset.exclude(status='DRAFT')
+        return queryset
+
+
+    @action(detail=True, methods=['post'])
+    def add_favorite(self, request, pk=None):
+        advertisement = self.get_object()
+        if advertisement.creator == request.user:
+            return Response({'Ошибка': 'Не возможно добавить в избранное собственное объявление'})
+        favorite = Favorite.objects.create(user=request.user, advertisement=advertisement)
+        serializer = FavoriteSerializer(favorite)
+        return Response(serializer.data)
+
+
+    @action(detail=False, methods=['get'])
+    def favorites(self, request):
+        favorites = Favorite.objects.filter(user=request.user)
+        advertisements = [favorite.advertisement for favorite in favorites]
+        serializer = AdvertisementSerializer(advertisements, many=True)
+        return Response(serializer.data)
+
